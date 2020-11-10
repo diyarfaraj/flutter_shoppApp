e5838f6584fe52e35584b04ba411ec7102d4a064
diyarfaraj <diyar.faraj@gmail.com>
2020-11-10 20:35:17 +0100

    improved provider

--- lib/providers/product_provider.dart
+++ lib/providers/product_provider.dart
@@ -73,6 +73,10 @@ class Products with ChangeNotifier {
     return [..._items];
   }
 
+  Product findById(String id) {
+    return _items.firstWhere((p) => p.id == id);
+  }
+
   void addProducts() {
     //fhheh
     notifyListeners();
